0e6f676bdfb2f7522c5be2b15048eb2bf59f9987
Asif Khan <asifkhan.github@gmail.com>
2018-10-20 16:52:51 +0600

    method created for showing custom toast

--- app/src/main/java/com/example/asifkhan/bookgalleryfirebase/helpers/Config.java
+++ app/src/main/java/com/example/asifkhan/bookgalleryfirebase/helpers/Config.java
@@ -1,4 +1,22 @@
 package com.example.asifkhan.bookgalleryfirebase.helpers;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.asifkhan.bookgalleryfirebase.R;
+
 public class Config {
+
+    //show custom success toast
+    public static void showToast(String message,Context context){
+        LayoutInflater layoutInflater=(LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view=layoutInflater.inflate(R.layout.custom_toast_layout,null);
+        Toast toast=new Toast(context);
+        toast.setText(message);
+        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.setView(view);
+        toast.show();
+    }
 }
