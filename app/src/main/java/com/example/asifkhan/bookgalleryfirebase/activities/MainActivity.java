baf1af0dbe640bf283fdc7a093e0d3814fc0708d
Asif Khan <asifkhan.github@gmail.com>
2018-10-20 16:51:20 +0600

    file modified

--- app/src/main/java/com/example/asifkhan/bookgalleryfirebase/activities/MainActivity.java
+++ app/src/main/java/com/example/asifkhan/bookgalleryfirebase/activities/MainActivity.java
@@ -1,12 +1,11 @@
 package com.example.asifkhan.bookgalleryfirebase.activities;
 
+
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.widget.GridView;
-import android.widget.Toast;
 
 import com.example.asifkhan.bookgalleryfirebase.R;
 import com.example.asifkhan.bookgalleryfirebase.adapters.BookGalleryAdapter;
