22c61bc54a62f31cc07c3dfc03a742d51e706cd6
Asif Khan <asifkhan.github@gmail.com>
2018-10-20 15:47:18 +0600

    neccessary instances created

--- app/src/main/java/com/example/asifkhan/bookgalleryfirebase/activities/MainActivity.java
+++ app/src/main/java/com/example/asifkhan/bookgalleryfirebase/activities/MainActivity.java
@@ -1,17 +1,34 @@
 package com.example.asifkhan.bookgalleryfirebase.activities;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.GridView;
+import android.widget.Toast;
 
 import com.example.asifkhan.bookgalleryfirebase.R;
+import com.example.asifkhan.bookgalleryfirebase.adapters.BookGalleryAdapter;
+import com.example.asifkhan.bookgalleryfirebase.models.Book;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private GridView gallery;
+    private BookGalleryAdapter bookGalleryAdapter;
+    private ArrayList<Book> books;
+    private FloatingActionButton addBook;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setSupportActionBar((Toolbar)findViewById(R.id.toolbar));
+        gallery=(GridView)findViewById(R.id.book_gallery);
+        books=new ArrayList<>();
+        bookGalleryAdapter=new BookGalleryAdapter(books,this);
+        addBook=(FloatingActionButton)findViewById(R.id.add_book);
+        gallery.setAdapter(bookGalleryAdapter);
     }
 }
